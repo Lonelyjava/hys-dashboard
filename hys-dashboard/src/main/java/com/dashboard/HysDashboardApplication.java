d46fb3fee97a998da2cfc19b9ef806225fd6ad73
Lonelyjava <kunk8667@gmail.com>
2022-11-27 16:55:27 +0530

    some changes

--- hys-dashboard/src/main/java/com/dashboard/HysDashboardApplication.java
+++ hys-dashboard/src/main/java/com/dashboard/HysDashboardApplication.java
@@ -1,5 +1,5 @@
 package com.dashboard;
-
+//kundan kumar
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
